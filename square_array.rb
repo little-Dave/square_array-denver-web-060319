929afa32064c77806ed0d4ec6f7a13c00a4a0a37
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-01 12:30:52 -0600

    Square each element in an array; return result
    Use #each to iterate through an array, square each element and push the result to a new array; return the new array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
 def square_array(array)
   # your code here
+  squared_array = []
+  array.each do |number|
+    squared_array.push(number ** 2)
+  end
+  squared_array
 end
+
+def square_array_advncd(array)
+  # #collect returns a new array with updated values
+  squared_array_advncd = array.collect do |number|
+    number ** 2
+  end
+  puts array.join(", ")
+  puts squared_array_advncd.join(", ")
+end
+
+square_array_advncd([2, 4, 6, 8])
